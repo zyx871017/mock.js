28ed2dbfe57be5945e599db9e4a37c14f97e18fd
zhaoyuxiang <112408584@qq.com>
2019-05-10 17:24:59 +0800

    fix mock to es5 language

--- mock.js
+++ mock.js
@@ -49,6 +49,41 @@
  * **/
 
 (function () {
+    if (!Object.keys) {
+        Object.keys = (function () {
+            var hasOwnProperty = Object.prototype.hasOwnProperty,
+                hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+                dontEnums = [
+                    'toString',
+                    'toLocaleString',
+                    'valueOf',
+                    'hasOwnProperty',
+                    'isPrototypeOf',
+                    'propertyIsEnumerable',
+                    'constructor'
+                ],
+                dontEnumsLength = dontEnums.length;
+
+            return function (obj) {
+                if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object');
+
+                var result = [];
+
+                for (var prop in obj) {
+                    if (hasOwnProperty.call(obj, prop)) result.push(prop);
+                }
+
+                if (hasDontEnumBug) {
+                    for (var i=0; i < dontEnumsLength; i++) {
+                        if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
+                    }
+                }
+                return result;
+            }
+        })()
+    }
+
+
     var SPECIAL_SYMBOL = '~`!@#$%^&*()_+-=[]{}<>?/';
 
     function getDataFromRange(dataRange, fixed) {
@@ -83,7 +118,9 @@
     }
 
     function initModalData(obj) {
-
+        var count = obj.count;
+        var dataObj = obj.data;
+        var keys = Object.keys(dataObj);
     }
 
     function initAllData(config) {
